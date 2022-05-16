7d6f89ce1e56a07678ad659109b3f32d2b2378ed
Ahmed Bodiwala <ahmed_bodi@msn.com>
2022-05-16 14:21:51 +0100

    build: revert depends changes

--- depends/packages/attr.mk
+++ depends/packages/attr.mk
@@ -4,27 +4,6 @@ $(package)_download_path=http://download.savannah.gnu.org/releases/attr/
 $(package)_file_name=$(package)-$($(package)_version).src.tar.gz
 $(package)_sha256_hash=25772f653ac5b2e3ceeb89df50e4688891e21f723c460636548971652af0a859
 
-define $(package)_set_vars
-#$(package)_config_opts_arm_linux += -platform linux-g++ -xplatform bitcoin-linux-g++
-#$(package)_config_opts_i686_linux  = -xplatform linux-g++-32
-#$(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
-#$(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
-#$(package)_config_opts_powerpc64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
-#$(package)_config_opts_powerpc64le_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
-#$(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
-#$(package)_config_opts_s390x_linux = -platform linux-g++ -xplatform bitcoin-linux-g++
-#  $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
-#  $(package)_config_opts_linux=--with-pic
-#  $(package)_config_opts_x86_64_linux=linux-x86_64
-#  $(package)_config_opts_i686_linux=linux-generic32
-#  $(package)_config_opts_arm_linux=linux-generic32
-#  $(package)_config_opts_aarch64_linux=linux-generic64
-#  $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
-#  $(package)_config_opts_mipsel_linux=linux-generic32
-#  $(package)_config_opts_mips_linux=linux-generic32
-#  $(package)_config_opts_powerpc_linux=linux-generic32
-endef
-
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,20 +1,20 @@
-packages:=boost libevent openssl
+packages:= zlib boost openssl libevent zeromq
+native_packages := native_ccache
 
-qt_packages = zlib
+qt_native_packages = native_protobuf
+qt_packages = qrencode protobuf zlib
 
-qrencode_packages = qrencode
 
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libseccomp libcap attr
-qt_android_packages=qt
+linux_packages:=libseccomp libcap attr
 
-rapidcheck_packages = rapidcheck
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
+
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 wallet_packages=bdb
-
-zmq_packages=zeromq
 
 upnp_packages=miniupnpc
 
